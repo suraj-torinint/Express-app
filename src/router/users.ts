347f00789b9bca6c3ecadbe140d5756d4127b225
suraj-torinit <suraj.mahamuni@torinit.ca>
2022-11-30 16:46:42 +0530

    implemented password encryption

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from "bcrypt";
 
 const userRoutes: express.Router = express.Router();
 
@@ -8,10 +9,21 @@ userRoutes.get("/", (req: express.Request, res: express.Response) => {
 
 userRoutes.post("/login", (req: express.Request, res: express.Response) => {
   // to receive form data
-  let formdata: {} = req.body;
+  let formdata = req.body;
   res.status(200).json({
     msg: "Form data received successfully",
     formdata: formdata,
+  });
+});
+
+userRoutes.post("/register", async (req: express.Request, res: express.Response) => {
+  // to get the form data from user
+  let { name, email, password } = req.body;
+  // encrypt password
+  let hashPassword = await bcrypt.hash(password, 10);
+  res.status(200).json({
+    user: { name, email, password },
+    hashPassword: hashPassword,
   });
 });
 
