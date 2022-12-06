95f05917636f61f03b0cad97ad8f162993163842
suraj-torinit <suraj.mahamuni@torinit.ca>
2022-12-06 10:32:17 +0530

    done with get an asset and get active assets

--- src/router/assets.ts
+++ src/router/assets.ts
@@ -20,4 +20,41 @@ router.get("/", (req: express.Request, res: express.Response) => {
     });
 });
 
+router.get("/is_active", (req: express.Request, res: express.Response) => {
+  db("assets")
+    .select("name", "hdd", "ssd", "status", "ram", "processor", "operating_system")
+    .where("is_active", "=", "0")
+    .then((data) => {
+      res.status(200).json({
+        message: "Data received successfully",
+        data: data,
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: "Error occured while fetching assets!",
+        errorMsg: error,
+      });
+    });
+});
+
+router.get("/:asset_id", (req: express.Request, res: express.Response) => {
+  const id: string = req.params.asset_id;
+  db.select("*")
+    .from("assets")
+    .where("assetId", "=", id)
+    .then((data) =>
+      res.status(200).json({
+        message: "Data received successfully",
+        data: data,
+      }),
+    )
+    .catch((error) => {
+      res.status(400).json({
+        error: "Error occured while fetching assets!",
+        errorMsg: error,
+      });
+    });
+});
+
 export default router;
